6f3d4776e7308514a09b3f076e612b541c41d179
Ronan Casey <rojocas@gmail.com>
2021-05-31 14:28:19 +0100

    Updated selector tag in nav link query selector method

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,7 @@
 const navSlide = () => {
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav-links');
-  const navLinks = document.querySelectorAll('.nav-links li:not(.slide-links)');
+  const navLinks = document.querySelectorAll('.nav-links li:not(.lg-screen-links)');
   
   burger.addEventListener('click', () => {
     // Toggle Nav
